114abb05c7f4c9640854e2dd746484882ae34fcc
itc-s26009 <s26009@std.it-college.ac.jp>
2026-06-17 07:56:27 +0000

    チャレンジ3まで終了

---
+++ Chapter06/challenge6-01.py
@@ -0,0 +1,5 @@
+name = "カミュ"
+
+print(name[0])  # 0番目：「カ」
+print(name[1])  # 1番目：「ミ」
+print(name[2])  # 2番目：「ュ」
---
+++ Chapter06/challenge6-03.py
@@ -0,0 +1,5 @@
+# (1) 任意の変数に問題文の文字列をセットする
+text = "aldous Huxlay was born in 1894."
+
+# (2) 変数をprint文で表示する際に、最初の1文字だけ大文字にするメソッドを使う
+print(text.capitalize())
